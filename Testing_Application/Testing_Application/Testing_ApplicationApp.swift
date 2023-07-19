cb4335d3e49fe417c36841f2a343c3f74768589f
Abhinav Anurag <11316570+abhinavanurag111@users.noreply.github.com>
2023-07-19 22:19:59 +0530

    Testo Commit Spaderma

---
+++ Testing_Application/Testing_Application/Testing_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_ApplicationApp.swift
+//  Testing_Application
+//
+//  Created by Abhinav Anurag on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Testing_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
